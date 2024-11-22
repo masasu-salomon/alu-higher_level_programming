17e2793bdf0ce7ad81b9f92ddf9e3116fa7fa169
masasu-salomon <s.masasuuwi@alustudent.com>
2024-11-22 12:19:33 -0800

    A

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Python script that fetchs."""
-
-
+"""Fetches url
+using the request moduel
+"""
 import requests
+
+
 if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
